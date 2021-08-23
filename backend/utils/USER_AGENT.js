56052741f6aef8e99d0a23b2a551acb6a3ceb560
艾琳 <85423779+EylinX@users.noreply.github.com>
2021-08-24 00:57:58 +0800

    Update USER_AGENT.js

--- backend/utils/USER_AGENT.js
+++ backend/utils/USER_AGENT.js
@@ -48,7 +48,7 @@ function randomNumber(min = 0, max = 100) {
 const RANDOM_UA = USER_AGENTS[randomNumber(0, USER_AGENTS.length)];
 
 const GET_RANDOM_TIME_UA = () => {
-  return 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0';
+  return 'set-ua Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36 Edg/91.0.864.59 UCBrowser/13.4.2.1122';
 };
 
 module.exports = {
